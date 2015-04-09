c54679e6bf65651b8b3ff0457bd7863b5e29ccdc
Łukasz Biedak <l.biedak@wp.pl>
2015-04-09 23:36:19 +0100

    [GUI] First picture, menu and layout

--- Agricoler/Agricoler.Designer.cs
+++ Agricoler/Agricoler.Designer.cs
@@ -28,34 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.mainMenu = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mainLayout = new System.Windows.Forms.TableLayoutPanel();
+            this.mainBoard = new System.Windows.Forms.PictureBox();
+            this.mainMenu.SuspendLayout();
+            this.mainLayout.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.mainBoard)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // mainMenu
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(621, 412);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.mainMenu.GripMargin = new System.Windows.Forms.Padding(1, 1, 0, 1);
+            this.mainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.mainMenu.Location = new System.Drawing.Point(0, 0);
+            this.mainMenu.Name = "mainMenu";
+            this.mainMenu.Padding = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.mainMenu.Size = new System.Drawing.Size(620, 24);
+            this.mainMenu.TabIndex = 1;
+            this.mainMenu.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.newGameToolStripMenuItem.Text = "New Game";
+            // 
+            // mainLayout
+            // 
+            this.mainLayout.AutoSize = true;
+            this.mainLayout.ColumnCount = 4;
+            this.mainLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.mainLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 320F));
+            this.mainLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 300F));
+            this.mainLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.mainLayout.Controls.Add(this.mainBoard, 1, 0);
+            this.mainLayout.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainLayout.Location = new System.Drawing.Point(0, 24);
+            this.mainLayout.Name = "mainLayout";
+            this.mainLayout.RowCount = 2;
+            this.mainLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.mainLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.mainLayout.Size = new System.Drawing.Size(620, 460);
+            this.mainLayout.TabIndex = 2;
+            // 
+            // mainBoard
+            // 
+            this.mainBoard.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainBoard.Image = global::Agricoler.Properties.Resources.main;
+            this.mainBoard.Location = new System.Drawing.Point(0, 0);
+            this.mainBoard.Margin = new System.Windows.Forms.Padding(0);
+            this.mainBoard.Name = "mainBoard";
+            this.mainLayout.SetRowSpan(this.mainBoard, 2);
+            this.mainBoard.Size = new System.Drawing.Size(320, 460);
+            this.mainBoard.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.mainBoard.TabIndex = 0;
+            this.mainBoard.TabStop = false;
             // 
             // Agricoler
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(646, 437);
-            this.Controls.Add(this.pictureBox1);
+            this.ClientSize = new System.Drawing.Size(620, 484);
+            this.Controls.Add(this.mainLayout);
+            this.Controls.Add(this.mainMenu);
+            this.MainMenuStrip = this.mainMenu;
+            this.MaximumSize = new System.Drawing.Size(1256, 982);
+            this.MinimumSize = new System.Drawing.Size(636, 522);
             this.Name = "Agricoler";
             this.Text = "Agricoler";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.mainMenu.ResumeLayout(false);
+            this.mainMenu.PerformLayout();
+            this.mainLayout.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.mainBoard)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip mainMenu;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.TableLayoutPanel mainLayout;
+        private System.Windows.Forms.PictureBox mainBoard;
     }
 }
 
