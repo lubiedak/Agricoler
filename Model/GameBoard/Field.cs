76d3e17eb31f458d6dfd029d13c4f776f63182f7
Łukasz Biedak <l.biedak@wp.pl>
2015-04-12 10:19:08 +0100

    [Model] bad keyword in Field class

--- Model/GameBoard/Field.cs
+++ Model/GameBoard/Field.cs
@@ -10,9 +10,9 @@ namespace Model
     {
         public int id { get; private set; }
         public String name { get; private set; }
-        public Action action { get; private set; } 
+        public Action action { get; private set; }
 
-        public abstract void DoAction(Player p)
+        public virtual void DoAction(Player p)
         {
 
         }
