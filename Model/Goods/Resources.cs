f22e08910f4b65912cfb7e7297f1c3478c2bbde5
Łukasz Biedak <l.biedak@wp.pl>
2015-04-12 11:41:59 +0100

    [Model] Resources class improvements

--- Model/Goods/Resources.cs
+++ Model/Goods/Resources.cs
@@ -8,17 +8,80 @@ namespace Model
 {
     public class Resources
     {
-        int _wood;
-        int _stone;
-        int _reed;
+        public int _wood { get; private set; }
+        public int _stone { get; private set; }
+        public int _reed { get; private set; }
 
         public Resources()
         {
-            _wood = 0;
+            _wood  = 0;
             _stone = 0;
-            _reed = 0;
+            _reed  = 0;
         }
 
-        public void AddResources();
+        public Resources(int wood, int stone, int reed)
+        {
+            _wood  = wood;
+            _stone = stone;
+            _reed  = reed;
+        }
+
+        public Resources(Resources r)
+        {
+            _wood  = r._wood;
+            _stone = r._stone;
+            _reed  = r._reed;
+        }
+
+        public void Add(int wood, int stone, int reed)
+        {
+            _wood  += wood;
+            _stone += stone;
+            _reed  += reed;
+        }
+        
+        public void Add(Resources r)
+        {
+            _wood  += r._wood;
+            _stone += r._stone;
+            _reed  += r._reed;
+        }
+
+        public bool isEnough(int wood, int stone, int reed)
+        {
+            return _wood >= wood && _stone >= stone && _reed >= reed;
+        }
+
+        public bool isEnough(Resources r)
+        {
+            return _wood >= r._wood && _stone >= r._stone && _reed >= r._reed;
+        }
+
+        public void Use(int wood, int stone, int reed)
+        {
+            if (isEnough(wood, stone, reed))
+            {
+                _wood  -= wood;
+                _stone -= stone;
+                _reed  -= reed;
+            }
+        }
+
+        public void Use(Resources r)
+        {
+            if (isEnough(r))
+            {
+                _wood  -= r._wood;
+                _stone -= r._stone;
+                _reed  -= r._reed;
+            }
+        }
+
+        public void UseAll()
+        {
+            _wood  = 0;
+            _stone = 0;
+            _reed  = 0;
+        }
     }
 }
