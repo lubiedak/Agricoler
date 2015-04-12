9c6569e33615d493ab12bd9821e7d45ba1e07833
Łukasz Biedak <l.biedak@wp.pl>
2015-04-12 10:20:55 +0100

    [Model] Changing accessors to classes Action and Fields

--- Model/GameBoard/ActionField.cs
+++ Model/GameBoard/ActionField.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Model.GameBoard
 {
-    class ActionField
+    public class ActionField
     {
     }
 }
--- Model/GameBoard/ResourcesField.cs
+++ Model/GameBoard/ResourcesField.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Model.GameBoard
 {
-    class ResourcesField
+    public class ResourcesField
     {
     }
 }
