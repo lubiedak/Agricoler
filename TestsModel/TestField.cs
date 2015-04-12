3c1d45d13b715dc16254bd45fe0d90d9f2d073b3
Szymon Sochocki <fide.simon@gmail.com>
2015-04-12 13:23:38 +0100

    Test project addedd with a starting class TestField

---
+++ TestsModel/TestField.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestsModel
+{
+    [TestClass]
+    public class TestField
+    {
+        [TestMethod]
+        public void ThisIsAlwaysTrue()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void ThisIsNotTrue()
+        {
+            Assert.IsTrue(false);
+        }
+    }
+}
